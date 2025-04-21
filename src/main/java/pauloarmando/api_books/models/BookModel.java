b4472500bdf3cefa3c1661c23fa216af86532e0f
paulovictorarmando <paulovictorarmando@gmail.com>
2025-04-21 11:35:26 -0400

    Creating my model

--- src/main/java/pauloarmando/api_books/models/BookModel.java
+++ src/main/java/pauloarmando/api_books/models/BookModel.java
@@ -1,4 +1,26 @@
 package pauloarmando.api_books.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class BookModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer release;
+    private Integer isbn;
+    private String gender;
+    private String publisher;
+    private String edition;
 }
