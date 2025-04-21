402fa637bb8f4ba7c4e3abc900c6ec879b4f4d36
paulovictorarmando <paulovictorarmando@gmail.com>
2025-04-21 10:26:34 -0400

    Config a project

---
+++ src/test/java/pauloarmando/api_books/ApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package pauloarmando.api_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
