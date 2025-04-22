d5443079afa6cb3d70ef82a3bc236e13c35c844a
paulovictorarmando <paulovictorarmando@gmail.com>
2025-04-22 06:05:03 -0400

    Repository create for books

--- src/main/java/pauloarmando/api_books/repository/BookRepository.java
+++ src/main/java/pauloarmando/api_books/repository/BookRepository.java
@@ -1,4 +1,13 @@
 package pauloarmando.api_books.repository;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pauloarmando.api_books.model.BookModel;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookModel, Long> {
+    Optional<BookModel> findByTitle(String title);
+    Optional<BookModel> findByIsbn(String isbn);
 }
